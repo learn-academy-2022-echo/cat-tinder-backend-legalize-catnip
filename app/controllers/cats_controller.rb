453a29d20e056ecaf88bb21b3ddf4a2d80c3a776
TuanLeVietz <tuanz_z@yahoo.com>
2022-09-22 16:27:03 -0700

    back end structure completed

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,16 @@
+class CatsController < ApplicationController
+def index
+    cats = Cat.all
+    render json: cats
+end
+
+def create
+end
+
+def update
+end
+
+def destroy
+end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+cats = [
+    {
+    name:"Puss in Boots",
+    age:7,
+    enjoys:"dueling",
+    image:"https://images.immediate.co.uk/production/volatile/sites/3/2022/03/Puss-in-Boots-2--b3ae519-e1647428302345.png?resize=620,413?quality=90&webp=true&resize=620,414"
+},
+    {
+    name:"Kaat",
+    age:3,
+    enjoys:"mewoing",
+    image:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nationalgeographic.com%2Fanimals%2Farticle%2Fanimals-cats-training-pets&psig=AOvVaw2SdGHiSBHnCV7lf19R-hAE&ust=1663972158689000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCNjLwr-5qfoCFQAAAAAdAAAAABAD"
+},
+    ]
+
+cats.each do |indiv_cat|
+    Cat.create indiv_cat
+    p "A new cat named #{indiv_cat} was added."
+end
+
+p Cat.all
